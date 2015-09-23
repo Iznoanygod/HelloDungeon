0dcc134fadd0eb4aefbfb723e99031053dc00266
Andrew Park <apark18@users.noreply.github.com>
2015-09-22 22:29:36 -0400

    Update HelloDungeon2.java

--- HelloDungeon2/HelloDungeon2.java
+++ HelloDungeon2/HelloDungeon2.java
@@ -1,27 +1,10 @@
 package main;
-import java.util.Random;
+import java.io.IOException;
 
 public class HelloDungeon2 {
 	//main
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(Math.ceil(202 / 100.0));
+	public static void main(String[] args) throws IOException{
+		save.Save(1);
 	}
-	public static float damage (float attack, float defence, float level, float base){
-		float damage;
-		float modifier;
-		Random rn = new Random();
-		float random = rn.nextInt(15) + 85;
-		float critical = rn.nextInt(2) + 1;
-		modifier = random / 100;
-		modifier = modifier * critical;
-			damage = 2 * level + 10;
-			damage = damage / 250;
-			damage = damage * attack / defence;
-			damage = damage * base;
-			damage = damage + 2;
-			damage = damage * modifier;
-		return damage;
-	}
-
 }
+
