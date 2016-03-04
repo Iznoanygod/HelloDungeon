abbc2982ff2cf3e9834cc2d816f2f85a3efa1930
Andrew Park <apark18@users.noreply.github.com>
2016-03-04 09:25:38 -0500

    Update save.java

--- HelloDungeon2/save.java
+++ HelloDungeon2/save.java
@@ -1,11 +1,42 @@
 package main;
- 
- 
- 
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class save {
 	
+	public static boolean vault(float[] itemID){
+		try
+	      {
+			boolean initiate = false;
+			int number = 0;
+	         FileOutputStream fileOut = new FileOutputStream("../saves/vault.save");
+	         ObjectOutputStream out = new ObjectOutputStream(fileOut);
+	         while(initiate == false){
+	        	 out.writeObject(itemID[number]);
+	        	 
+	        	 number++;
+	        	 if(number > 99){
+		    		  initiate = true;
+		    	  }
+	         }
+	         out.close();
+	         fileOut.close();
+	         
+	      }catch(IOException i)
+	      {
+	          i.printStackTrace();
+	      }
+		return true;
+	}
 	
 	
 	
-    }
-    
+	
+	
+	
+	
+	
+	
+}
