67c682c0e97c399d0ebf1a50510a38c84abf18e8
Andrew Park <apark18@users.noreply.github.com>
2016-10-17 22:04:23 -0400

    Add files via upload

---
+++ HelloDungeon2/main/battle/Damage.java
@@ -0,0 +1,22 @@
+package main.battle;
+
+import java.util.Random;
+
+public class Damage{
+
+	public static float damage (float attack, float defence, float level, float base){
+		float damage;
+		float modifier;
+		Random rn = new Random();
+		float random = rn.nextInt(15) + 85;
+		modifier = random / 100;
+			damage = 2 * level + 10;
+			damage = damage / 250;
+			damage =  (damage * attack / defence);
+			damage =  (damage * base);
+			damage = damage + 2;
+			damage =  (damage * modifier);
+		return (int) Math.ceil(damage);
+	}
+
+}
