8885d0e661f99ac01b74dda0d6a7b040a2a45b95
Andrew Park <apark18@users.noreply.github.com>
2016-10-19 20:36:36 -0400

    Add files via upload

--- HelloDungeon2/main/Welcome.java
+++ HelloDungeon2/main/Welcome.java
@@ -1,14 +1,7 @@
 package main;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
-
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 import main.items.Item;
 import main.window.Frame;
